faa3d89ce7fa24fa1c0e1776183d8536aae573a8
fikriks <fikriks130@gmail.com>
2023-07-31 14:58:09 +0700

    update: next config :rocket:

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  swcMinify: true,
+  experimental: { esmExternals: true },
 }
 
 module.exports = nextConfig
