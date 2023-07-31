7ac80b035a14dda8fdd6de8b65be8e768bfe8111
fikriks <fikriks130@gmail.com>
2023-07-31 15:09:58 +0700

    update: pages :rocket:

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+if (typeof window !== "undefined") {
+  require("bootstrap/dist/js/bootstrap");
 }
+import '../styles/index.scss';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+        <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
